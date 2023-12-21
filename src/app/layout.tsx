61bf2e0531d7a1855eab12374b657d39f3444008
Wilembergson Alfredo da Silva <56082029+wilembergson@users.noreply.github.com>
2023-12-21 00:05:02 -0300

    feat: Dockerfile adicionado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-          <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
       </head>
       <body className={inter.className}>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
         <GlobalContextProvider>
           {children}
         </GlobalContextProvider>
